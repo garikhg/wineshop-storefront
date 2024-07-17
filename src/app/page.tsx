10dd294e436bbcbfd673e6b9e734cd59fa88ffe3
garikhg <garik.28h@gmail.com>
2024-07-17 20:54:45 +0400

    Update header and page details
    The header in the application has been significantly updated, adding substantial styling and navigation options. Removed the button in page.tsx, corrected the incorrect path in index.tsx. Updated fonts and metadata details in layout.tsx to reflect the brand more accurately.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,6 @@ export default function Home() {
     return (
         <>
             <div className="max-w-screen-2xl mx-auto px-6">
-
-                <Button>
-                    Button
-                </Button>
-
                 Homepage
             </div>
         </>
