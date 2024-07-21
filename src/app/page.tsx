cf508c83a43d1275bcf9f90e65b32450b5cb1109
garikhg <garik.28h@gmail.com>
2024-07-21 21:54:55 +0400

    Add HeroSlider component to homepage
    A new HeroSlider component has been created and added to the homepage. This component has also been exported through the central index.tsx in the components directory for future usage on different pages of the website.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import AnimatedComponent from "@/components/AnimatedComponent";
+import {HeroSlider} from "@/components";
 
 export default function Home() {
     return (
         <>
             <div className="max-w-screen-2xl mx-auto px-6">
+                <HeroSlider />
+
                 Homepage
             </div>
         </>
---
+++ src/components/HeroSlider/HeroSlider.tsx
@@ -0,0 +1,1 @@
+import React from 'react';const HeroSlider = () => {    return (        <div>            Hero Slider        </div>    );};export default HeroSlider;
